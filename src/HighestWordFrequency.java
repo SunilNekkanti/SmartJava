3bb79590ec0b91b21eddb0f4549bdc35dd063858
sunil <venky.sunil@gmail.com>
2018-08-04 09:22:09 -0700

    clean code

--- src/HighestWordFrequency.java
+++ src/HighestWordFrequency.java
@@ -5,43 +5,74 @@ import java.util.stream.Stream;
 
 public class HighestWordFrequency {
 
+    static List<String> retrieveMostFrequentlyUsedWords(String literatureText, List<String> wordsToExclude){
 
+        literatureText = literatureText.replaceAll("[^a-zA-Z0-9\\s]","");
+        String[] allwords = literatureText.split("\\s");
+        List<String> filteredList = new ArrayList<String>();
+        List<String> resultList = new ArrayList<String>();
+
+        for(String word: allwords){
+            if(!wordsToExclude.stream().anyMatch(word::equalsIgnoreCase))
+                filteredList.add(word);
+        }
+
+
+        Map<String, Long> hm = filteredList.stream().collect(Collectors.groupingBy(String::toString, Collectors.counting()));
+
+
+        Long max = Collections.max(hm.values());
+
+        for(Map.Entry<String, Long> me: hm.entrySet()){
+            if(me.getValue().equals(max)){
+                resultList.add(me.getKey());
+            }
+
+        }
+
+
+
+        return resultList;
+    }
     public static void main(String[] s){
 
 
         String phrase = new String();
-        phrase="jack and jill, went up the hill and jack fell down" +
-                "ok na" +
+        phrase="Jack and Jill, went up the hill and jack fell down ok na jam jam " +
+                "ok na " +
                 "mari";
+        List<String> wordsToExclude = Arrays.asList("and", "the", "he", "to", "is", "Jack", "Jill");
 
-        String[] myw = phrase.split(" ");
+        System.out.println(retrieveMostFrequentlyUsedWords(phrase, wordsToExclude));
 
-
-
-        Map<String, Integer> fm = new HashMap<String, Integer>();
-
-
-
-        for(String so : myw)
-        {
-            fm.put(so, Collections.frequency(Arrays.asList(myw), so));
-
-        }
-        System.out.println(Collections.max(fm.values()));
-        int y = Collections.max(fm.values());
-        System.out.println(fm.entrySet().stream()
-                .sorted(Collections.reverseOrder(Comparator.comparing(Map.Entry<String, Integer>::getValue)))
-                .filter(e -> e.getValue()==y)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList()).toString());
-
-        System.out.println(fm
-                .entrySet()
-                .stream()
-                .filter(e -> e.getValue().equals(Collections.max(fm.values())))
-                .map(e -> e.getKey())
-                .collect(Collectors.toList()).toString());
-
+//        String[] myw = phrase.split(" ");
+//
+//
+//
+//        Map<String, Integer> fm = new HashMap<String, Integer>();
+//
+//
+//
+//        for(String so : myw)
+//        {
+//            fm.put(so, Collections.frequency(Arrays.asList(myw), so));
+//
+//        }
+//        System.out.println(Collections.max(fm.values()));
+//        int y = Collections.max(fm.values());
+//        System.out.println(fm.entrySet().stream()
+//                .sorted(Collections.reverseOrder(Comparator.comparing(Map.Entry<String, Integer>::getValue)))
+//                .filter(e -> e.getValue()==y)
+//                .map(Map.Entry::getKey)
+//                .collect(Collectors.toList()).toString());
+//
+//        System.out.println(fm
+//                .entrySet()
+//                .stream()
+//                .filter(e -> e.getValue().equals(Collections.max(fm.values())))
+//                .map(e -> e.getKey())
+//                .collect(Collectors.toList()).toString());
+//
 //           Stream.of(myw).
 //                          forEach(e -> {
 //                              if(fm.containsKey(e)){
@@ -55,7 +86,8 @@ public class HighestWordFrequency {
 //              Collections.sort(af);
 //              Collections.reverse(af);
 //        System.out.println(af.toString());
-    }
+//
+}
 
 
 }
--- src/ReOrderingLines.java
+++ src/ReOrderingLines.java
@@ -1,29 +1,70 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReOrderingLines {
 
      static List<String> reorderLines(int logFileSize, List<String> logfile){
 
-        List<String> sortedWordList = logfile.stream()
-                                  .filter(num -> !(num.split(" ")[1]).chars().allMatch(Character::isDigit))
-                                  .sorted(Comparator.comparing( firstWord ->  firstWord.split(" ")[1]))
-                                  .collect(Collectors.toList());
+//        List<String> sortedWordList = logfile.stream()
+//                                  .filter(num -> !(num.split(" ")[1]).chars().allMatch(Character::isDigit))
+//                                  .sorted(Comparator.comparing( firstWord ->  firstWord.split(" ")[1]))
+//                                  .collect(Collectors.toList());
+//
+//        List<String> numberList = logfile.stream()
+//                .filter(num -> (num.split(" ")[1]).chars().allMatch(Character::isDigit))
+//                .collect(Collectors.toList());
+//
+//         System.out.println(logfile.stream()
+//                 .collect(Collectors.partitioningBy(num -> !(num.split(" ")[1]).chars().allMatch(Character::isDigit))));
+//
+//        sortedWordList.addAll(numberList);
 
-        List<String> numberList = logfile.stream()
-                .filter(num -> (num.split(" ")[1]).chars().allMatch(Character::isDigit))
-                .collect(Collectors.toList());
+         List<String> sortedWordList = logfile;
 
-         System.out.println(logfile.stream()
-                 .collect(Collectors.partitioningBy(num -> !(num.split(" ")[1]).chars().allMatch(Character::isDigit))));
+         List<String> numbersList = new ArrayList<String>();
+         List<String> wordsList = new ArrayList<String>();
 
-        sortedWordList.addAll(numberList);
+         for(String e:logfile){
+             if(e.split(" ")[1].matches("-?\\d+(\\.\\d+)?")){
+                 numbersList.add(e);
+             }
+             else {
+                 wordsList.add(e);
+             }
+         }
 
-        return sortedWordList;
+
+         wordsList.sort(new Comparator<String>() {
+             @Override
+             public int compare(String o1, String o2) {
+
+
+                 String identifier_1 = o1.split(" ")[0];
+                 String identifier_2 = o2.split(" ")[0];
+
+                 String o1_firststring = o1.split(" ")[1];
+                 String o2_firststring = o2.split(" ")[1];
+
+                 String o1_wholestring = o1.split(" ", 2)[1];
+                 String o2_wholestring = o2.split(" ", 2)[1];
+
+
+                 if(o1_wholestring.equals(o2_wholestring)){
+                     return identifier_1.compareTo(identifier_2);
+                 }
+
+                 else{
+                     return o1_firststring.compareTo(o2_firststring);
+                 }
+
+
+             }
+         });
+
+         wordsList.addAll(numbersList);
+        return wordsList;
     }
+
 
 
     public static void main(String[] f){
@@ -31,9 +72,13 @@ public class ReOrderingLines {
      int logFileSize = 5;
      List<String> loglines =  Arrays.asList("a1 9 2 3 1",
                                             "g1 Act Car",
+                                             "pz ruki huki",
                                              "zo4 4 7",
                                              "ab1 off KEY dog",
-                                             "a8 act zoo");
+                                             "a8 act zoo",
+              "a8 bat cat",
+             "z04 99 88",
+             "lz loki maki");
 
         List<String> sorted = reorderLines(logFileSize, loglines);
 
