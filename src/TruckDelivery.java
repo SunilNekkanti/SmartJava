910e82e3dc35762e6a2ed2ce89934dc1b83740c0
sunil <venky.sunil@gmail.com>
2019-01-05 20:02:10 -0700

    truck delivery complete

--- src/TruckDelivery.java
+++ src/TruckDelivery.java
@@ -1,47 +1,93 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import javafx.util.Pair;
 
 public class TruckDelivery {
 
-
     public static void main(String[] args) {
-
-        Pair<String, String> ps = new Pair<>("emara", "okna");
-
         List<List<Integer>> list = new ArrayList<List<Integer>>();
-        list.add(Arrays.asList(1,1,0));
         list.add(Arrays.asList(1,1,1));
-        list.add(Arrays.asList(1,9,0));
-        //minimumDistance(3,3, list);
-        getNeighbours("00", 3 ,3);
+        list.add(Arrays.asList(0,0,1));
+        list.add(Arrays.asList(1,9,1));
+        minimumDistance(3,3, list);
     }
 
-//    static int minimumDistance(int numRows, int numColumns, List<List<Integer>> area){
-//
-//        Queue<String> visitedGrids = new LinkedList<String>();
-//        Map<String,Boolean> traversedNodes = new HashMap<String, Boolean>();
-//        int currentRow = 0;
-//        int currentColumn = 0;
-//        String currentNode = getNode(currentRow, currentColumn);
-//        visitedGrids.add(currentNode);
-//        traversedNodes.put(currentNode, true);
-//
-//        while(!visitedGrids.isEmpty()){
-//              String node = visitedGrids.poll();
-//              List<String> neighbours = getNeighbours(node);
-//              // add node to visited in the map
-//              // add all neighbours to queue
-//
-//
-//
-//        }
-//
-//
-//
-//        return -1;
-//    }
+    static int minimumDistance(int numRows, int numColumns, List<List<Integer>> area){
+
+        Queue<String> unvisitedGrids = new LinkedList<String>();
+
+        Set<String> visitedGrids = new HashSet<String>();
+
+        Set<Pair<String, String>> parentChildSet = new HashSet<Pair<String, String>>();
+
+        List<String> neighbours = new ArrayList<String>();
+
+        String startNode = "00";
+
+        String destNode = "";
+
+        unvisitedGrids.add(startNode);
+        boolean destNotFound = true;
+
+        while(destNotFound && !unvisitedGrids.isEmpty()){
+
+            String currentNode = unvisitedGrids.poll();
+            visitedGrids.add(currentNode);
+
+            if (getData(currentNode,area) == 9) {
+                destNode = currentNode;
+                destNotFound = false;
+                break;
+            }
+            else{
+                neighbours = getNeighbours(currentNode,numRows, numColumns);
+
+                for(String node: neighbours){
+                    if(!visitedGrids.contains(node) && getData(node,area)!=0) {
+                        parentChildSet.add(new Pair(currentNode, node));
+                        unvisitedGrids.add(node);
+                    }
+                }
+            }
+        }
+
+        backTrackPath(destNode, startNode, parentChildSet, area);
+
+        return -1;
+
+
+    }
+
+    public static String backTrackPath(String destNode, String startNode, Set<Pair<String, String>> parentChildSet, List<List<Integer>> area){
+
+            boolean pathMapped = false;
+            StringBuilder sb = new StringBuilder(destNode);
+            StringBuilder datasb = new StringBuilder(String.valueOf(getData(destNode,area)));
+            String parent = "";
+            String endNode = destNode;
+
+            while(!pathMapped) {
+                parent = getParent(parentChildSet, endNode);
+                sb.append("-> "+parent);
+                datasb.append("-> "+String.valueOf(getData(parent,area)));
+                if(parent.equals(startNode)){
+                    pathMapped=true;
+                }
+                endNode = parent;
+
+            }
+        System.out.println(sb.toString());
+        System.out.println(datasb.toString());
+          return sb.toString();
+    }
+
+    private static String getParent(Set<Pair<String, String>> parentChildSet,String node){
+
+        return parentChildSet.stream().filter(f -> f.getValue().equals(node)).map(Pair::getKey).findAny().get();
+    }
 
     private static List<String> getNeighbours(String node, int numRows, int numColumns) {
         List<String> result = new ArrayList<String>();
@@ -56,21 +102,29 @@ public class TruckDelivery {
         up = row-1 < 0 ? "NA" : String.valueOf(row-1)+String.valueOf(column);
         down = row+1 >= numRows? "NA" : String.valueOf(row+1)+String.valueOf(column);
 
-        System.out.println(left);
-        System.out.println(right);
-        System.out.println(up);
-        System.out.println(down);
+        result.add(left);
+        result.add(right);
+        result.add(up);
+        result.add(down);
 
-        return result;
+        return result.stream().filter(isBlank()).collect(Collectors.toList());
     }
 
-    static int getDataForStringNode(String node,List<List<Integer>> area){
 
-        return 0;
+    public static Predicate<String> isBlank()
+    {
+        return data -> !data.equals("NA");
+    }
+
+
+    static int getData(String node,List<List<Integer>> area){
+        String[] p = node.split("");
+        int row = Integer.parseInt(p[0]);
+        int column = Integer.parseInt(p[1]);
+
+        int data = area.get(row).get(column);
+        return data;
 
     }
 
-    static String getNode(int row, int column){
-        return  String.valueOf(row)+String.valueOf(column);
-    }
 }
