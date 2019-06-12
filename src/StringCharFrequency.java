aefdb1aaa964d5aa3de57ac52f65c8bed34d677f
Sunil <snekkanti@dssolutions.com>
2019-06-12 14:50:49 -0700

    some more new files

--- src/StringCharFrequency.java
+++ src/StringCharFrequency.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toMap;
 
 public class StringCharFrequency {
 
@@ -13,8 +14,18 @@ public class StringCharFrequency {
 
         Map<Character,Long> maplong = mystr.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         Map<Character,Integer> mapint = mystr.chars().mapToObj(c -> (char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
-        Map<Character,Integer> mapint2 = mystr.chars().mapToObj(c -> (char)c).collect(Collectors.toMap(Function.identity(), c -> 1, Math::addExact));
+        Map<Character,Integer> mapint2 = mystr.chars().mapToObj(c -> (char)c).collect(toMap(Function.identity(), c -> 1, Math::addExact));
         mystr.chars().mapToObj(c -> (char)c).forEach(e->map.put(e, map.getOrDefault(e, 0) + 1));
+        mystr.chars().mapToObj(c -> (char)c).forEach(c->map.merge(c,1,Integer::sum)); //alternative
+        Map<Character, Integer> frequencies = mystr.chars().boxed()
+                                                   .collect(toMap(
+                                                             // key = char
+                                                             k -> Character.valueOf((char) k.intValue()),
+                                                             v -> 1,         // 1 occurence
+                                                             Integer::sum)); // counting
+
+
+        mystr.chars().mapToObj(c -> (char)c).forEach(c->map.merge(c,1,Integer::sum));
         System.out.println(map);
 
     }
